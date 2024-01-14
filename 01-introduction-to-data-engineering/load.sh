5ad1b59caea7a0651b4509ae5765806700832adb
apicharto <155867145+apicharto@users.noreply.github.com>
2024-01-14 07:24:39 +0000

    finish day 1

--- 01-introduction-to-data-engineering/load.sh
+++ 01-introduction-to-data-engineering/load.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-API_KEY='YOUR_API_KEY'
-COLLECTION_ID='YOUR_COLLECTION_ID'
+API_KEY='$2a$10$7wMPuPGt8k4DuZ9rF1oGV.oDVZqd6JbegECR3wzmA9r7jlUHt2Owi'
+COLLECTION_ID='659a4d0b1f5677401f189fee'
 
 curl -XPOST \
     -H "Content-type: application/json" \
     -H "X-Master-Key: $API_KEY" \
     -H "X-Collection-Id: $COLLECTION_ID" \
     -d @dogs.json \
-    "https://api.jsonbin.io/v3/b"
+    "https://api.jsonbin.io/v3/b"a  
